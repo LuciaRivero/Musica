46c678df2b6cfdbaf4b60d6348540072c3c554e2
Lucia Rivero <lucia_rivero@itrsa.com.ar>
2019-10-30 10:45:33 -0300

    agrego info del artista

--- src/App.js
+++ src/App.js
@@ -1,52 +1,67 @@
 import React, {useState, useEffect, Fragment} from 'react';
 import Formulario from './Components/Formulario';
-import axios from 'axios';
 import Cancion from './Components/Cancion';
+import Informacion from './Components/Informacion';
+import axios from 'axios';
 
 function App() {
+
+  // Utilizar useState con 3 states
   const [artista, agregarArtista] = useState('');
   const [letra, agregarLetra] = useState([]);
   const [info, agregarInfo] = useState({});
 
+  // Método para consultar la API de Letras de canciones
   const consultarAPILetra = async busqueda => {
-    const {artista, cancion} = busqueda;
+      const {artista, cancion} = busqueda;
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
 
-    const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+      // consultar la api
+      const resultado = await axios(url);
 
-    const resultado = await axios(url);
+      // almacenar el artista que se buscó
+      agregarArtista(artista);
 
-    agregarLetra(resultado.data.lyrics)
+      // almacenar la letra en el state
+      agregarLetra(resultado.data.lyrics);
   }
 
+  // Método para consultar la API de Información
   const consultarAPIInfo = async () => {
-    const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
-
-    const resultado = await axios(url);
-
-    agregarInfo(resultado.data.artist[0]);
+      if(artista){
+        const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+        const resultado = await axios(url);
+        agregarInfo(resultado.data.artists[0]);
+      }
   }
 
-  useEffect (() => {
-    consultarAPIInfo()
-  }, [artista])
-
+  useEffect(
+    () => {
+      consultarAPIInfo();
+    }, [artista]
+  )
 
   return (
     <Fragment>
-      <Formulario 
-        consultarAPILetra={consultarAPILetra}/>
+        <Formulario 
+          consultarAPILetra={consultarAPILetra}
+        />
         <div className="container mt-5">
           <div className="row">
-            <div className="col-md-6">
-
-            </div>
-            <div className="col-md-6">
-              <Cancion letra={letra}/>
-            </div>
+              <div className="col-md-6">
+                  <Informacion 
+                    info={info}
+                  />  
+              </div>
+              <div className="col-md-6">
+                  <Cancion 
+                      letra={letra}
+                  />
+              </div>
           </div>
         </div>
     </Fragment>
-  );
+  )
 }
 
 export default App;
